88a22861d8d70d8d591a39ebc8ed8a85d09a3798
Luan Martins <luan.martins@parfin.io>
2022-03-21 17:24:02 -0300

    Lecture 2

--- angular-basics/egghead-angular/src/app/app.component.ts
+++ angular-basics/egghead-angular/src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: 
+    `
+      <h1>{{title}}</h1>
+    `,
+  styles: [
+    `
+      h1 {
+        color: blue;
+      }
+    `
+  ]
 })
 export class AppComponent {
-  title = 'egghead-angular';
+  public title: string = 'egghead-angular';
 }
